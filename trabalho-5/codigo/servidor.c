fb734c3b17804dd37afd1b6dcea6441bfdef9304
Felipe Carvalho <felipesoares93@gmail.com>
2018-10-21 17:37:53 -0300

    Questao 2

--- trabalho-5/codigo/servidor.c
+++ trabalho-5/codigo/servidor.c
@@ -12,18 +12,18 @@
 #include "basic.h"
 #include "socket_helper.h"
 
-#define LISTENQ 10              
-#define MAXDATASIZE 4096         
+#define LISTENQ 10
+#define MAXDATASIZE 4096
 #define EXIT_COMMAND "exit\n"
 
 void doit(int connfd, struct sockaddr_in clientaddr);
 
 int main (int argc, char **argv) {
-   int    listenfd,              
-          connfd,               
-          port;                  
-   struct sockaddr_in servaddr;  
-   char   error[MAXDATASIZE + 1];     
+   int    listenfd,
+          connfd,
+          port;
+   struct sockaddr_in servaddr;
+   char   error[MAXDATASIZE + 1];
 
    if (argc != 2) {
       strcpy(error,"uso: ");
@@ -56,7 +56,7 @@ int main (int argc, char **argv) {
 
       if((pid = fork()) == 0) {
          Close(listenfd);
-         
+
          doit(connfd, clientaddr);
 
          Close(connfd);
@@ -66,27 +66,25 @@ int main (int argc, char **argv) {
 
       Close(connfd);
    }
-   
+
    return(0);
 }
 
 void doit(int connfd, struct sockaddr_in clientaddr) {
    char recvline[MAXDATASIZE + 1];
-   char   message[MAXDATASIZE + 1];      
-   int n;                  
+   char   message[MAXDATASIZE + 1];
+   int n;
    socklen_t remoteaddr_len = sizeof(clientaddr);
-   
+
    while ((n = read(connfd, recvline, MAXDATASIZE)) > 0) {
-      recvline[n] = 0; 
+      recvline[n] = 0;
 
       if (getpeername(connfd, (struct sockaddr *) &clientaddr, &remoteaddr_len) == -1) {
          perror("getpeername() failed");
          return;
       }
 
-      printf("<%s-%d>: %s\n", inet_ntoa(clientaddr.sin_addr),(int) ntohs(clientaddr.sin_port), recvline);
-
-      printf("Digite uma mensagem:\n");
+      // printf("Digite uma mensagem:\n");
       fgets(message, MAXDATASIZE, stdin);
       if(strcmp(message, EXIT_COMMAND) == 0) {
          break;
